d3c694555fc776aaf2c23e8ee5ef3476c58debeb
María José Rodríguez F <rodriguez.mariajose.f@gmail.com>
2017-05-04 23:39:32 -0300

    comienzo trabajo

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+function Squad(nombre, edad, hobbie){
+	this.nombre = nombre;
+	this.apellido = apellido;
+	this.edad = edad;
+	this.hobbie = hobbie;
+}
+
+var Camila Saez = new Squad("Camila Saez", 24, "Origami")
+var Antonia Cerda = new Squad("Antonia Cerda", 23, "Disenar")
+var Carolina Tapia = new Squad("Carolina Tapia", 27, "Leer")
+var Maria Jose Pozo = new Squad("Maria Jose Pozo", 31, "Bailar")
+var Francisca Ojeda = new Squad("Francisca Ojeda", 28, "Rollerderby")
+var Marion Castillo = new Squad("Marion Castillo", 29, "Caminar")
+var Maria Jose Rodriguez = new Squad("Maria Jose Rodriguez", 32, "Ver series")
+
+
+var ultimo = document.getElementById("Mostrar");
+var muestra = (hola.nombre + "que tal");
+ultimo.innerHTML = muestra;
