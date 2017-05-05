6a0f26fd393cae3c28faf6b22a811db5cef0b416
María José Rodríguez F <rodriguez.mariajose.f@gmail.com>
2017-05-05 09:02:54 -0300

    cambios

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,45 +3,16 @@ function Squad(nombre, edad, hobbie){
 	this.apellido = apellido;
 	this.edad = edad;
 	this.hobbie = hobbie;
-}
+	}
+};
 
-var Persona1 = new Squad("Camila Saez", 24, "Origami")
-var Persona2 = new Squad("Antonia Cerda", 23, "Disenar")
-var Persona3 = new Squad("Carolina Tapia", 27, "Leer")
-var Persona4 = new Squad("Maria Jose Pozo", 31, "Bailar")
-var Persona5 = new Squad("Francisca Ojeda", 28, "Rollerderby")
-var Persona6 = new Squad("Marion Castillo", 29, "Caminar")
-var Persona7 = new Squad("Maria Jose Rodriguez", 32, "Ver series")
+var Cami = new Squad("Camila", "Saez", 24, "Origami")
+var Anto = new Squad("Antonia", "Cerda", 23, "Disenar")
+var Caro = new Squad("Carolina", "Tapia", 27, "Leer")
+var Cote = new Squad("Maria Jose", "Pozo", 31, "Bailar")
+var Fran = new Squad("Francisca", "Ojeda", 28, "Rollerderby")
+var Mar = new Squad("Marion", "Castillo", 29, "Caminar")
+var cot = new Squad("Maria Jose", "Rodriguez", 32, "Ver series")
 
-
-var ultimo = document.getElementById("ver1");
-var muestra = ("Nombre: " + Persona1.nombre + "<br>" + "Apellido: " + Persona1.apellido + "<br>" + "Edad: " + Persona1.edad + "<br>" + "Hobbie: " + Persona1.hobbie);
-ultimo.innerHTML = muestra;
-
-var ultimo = document.getElementById("ver2");
-var muestra = ("Nombre: " + Persona2.nombre + "<br>" + "Apellido: " + Persona2.apellido + "<br>" + "Edad: " + Persona2.edad + "<br>" + "Hobbie: " + Persona2.hobbie);
-ultimo.innerHTML = muestra;
-
-var ultimo = document.getElementById("ver3");
-var muestra = ("Nombre: " + Persona3.nombre + "<br>" + "Apellido: " + Persona3.apellido + "<br>" + "Edad: " + Persona3.edad + "<br>" + "Hobbie: " + Persona3.hobbie);
-ultimo.innerHTML = muestra;
-
-var ultimo = document.getElementById("ver4");
-var muestra = ("Nombre: " + Persona4.nombre + "<br>" + "Apellido: " + Persona4.apellido + "<br>" + "Edad: " + Persona4.edad + "<br>" + "Hobbie: " + Persona4.hobbie);
-ultimo.innerHTML = muestra;
-
-var ultimo = document.getElementById("ver5");
-var muestra = ("Nombre: " + Persona5.nombre + "<br>" + "Apellido: " + Persona5.apellido + "<br>" + "Edad: " + Persona5.edad + "<br>" + "Hobbie: " + Persona5.hobbie);
-ultimo.innerHTML = muestra;
-
-var ultimo = document.getElementById("ver6");
-var muestra = ("Nombre: " + Persona6.nombre + "<br>" + "Apellido: " + Persona6.apellido + "<br>" + "Edad: " + Persona6.edad + "<br>" + "Hobbie: " + Persona6.hobbie);
-ultimo.innerHTML = muestra;
-
-var ultimo = document.getElementById("ver7");
-var muestra = ("Nombre: " + Persona7.nombre + "<br>" + "Apellido: " + Persona7.apellido + "<br>" + "Edad: " + Persona7.edad + "<br>" + "Hobbie: " + Persona7.hobbie);
-ultimo.innerHTML = muestra;
-
-
-
-
+var otro = document.getElementById("mostrar");
+muestra.inner.HTML += "Nombre: " + Cami.nombre + "<br>Edad: " +Cami.edad + "<br>Hobbie: ";
